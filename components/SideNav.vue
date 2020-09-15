1ce4ffb1202e76b0d0010113c22e9aa24a15185d
Alistair Quinn <alistairjoelquinn@gmail.com>
2020-09-15 21:41:18 +0200

    no-console disabled in sidenav and side links moved to v-for

--- components/SideNav.vue
+++ components/SideNav.vue
@@ -1,14 +1,56 @@
 <template>
     <div class="nav-container">
         <div class="logo">😼</div>
-        <div>Ethos</div>
-        <div>Projects</div>
-        <div>Portfolio</div>
-        <div>Testimonials</div>
-        <div>Services</div>
-        <div>Contact</div>
+        <div v-for="item in navItems" :key="item.name" class="nav-item">
+            {{ item.name }}
+        </div>
     </div>
 </template>
+
+<script>
+/* eslint-disable no-console */
+export default {
+    data() {
+        return {
+            navItems: [
+                {
+                    name: 'Ethos',
+                    route: '/ethos',
+                    color: 'blue',
+                },
+                {
+                    name: 'Projects',
+                    route: '/projects',
+                    color: 'white',
+                },
+                {
+                    name: 'Portfolio',
+                    route: '/portfolio',
+                    color: 'green',
+                },
+                {
+                    name: 'Testimonials',
+                    route: '/testimonials',
+                    color: 'pink',
+                },
+                {
+                    name: 'Services',
+                    route: '/services',
+                    color: 'orange',
+                },
+                {
+                    name: 'Contact',
+                    route: '/contact',
+                    color: 'dimgrey',
+                },
+            ],
+        }
+    },
+    mounted() {
+        console.log(this.navItems)
+    },
+}
+</script>
 
 <style scoped>
 .nav-container {
@@ -20,5 +62,12 @@
     width: 100%;
     font-size: 180px;
     text-align: center;
+}
+.nav-item {
+    text-align: center;
+    font-size: 35px;
+    transform: rotate(-30deg);
+    padding-bottom: 10px;
+    border-top: 1px solid black;
 }
 </style>
