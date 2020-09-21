3dfbb627ed795d5e6262ad8b63a5b375be176ffc
Alistair Quinn <alistairjoelquinn@gmail.com>
2020-09-21 15:10:40 +0200

    sass imported for targetting bulma variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,77 +1,50 @@
 export default {
-    /*
-     ** Nuxt rendering mode
-     ** See https://nuxtjs.org/api/configuration-mode
-     */
     mode: 'universal',
-    /*
-     ** Nuxt target
-     ** See https://nuxtjs.org/api/configuration-target
-     */
     target: 'server',
-    /*
-     ** Headers of the page
-     ** See https://nuxtjs.org/api/configuration-head
-     */
     head: {
-        title: process.env.npm_package_name || '',
+        title: 'Richard Catty - Writer',
         meta: [
             { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+            },
             {
                 hid: 'description',
                 name: 'description',
                 content: process.env.npm_package_description || '',
             },
         ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    },
-    /*
-     ** Global CSS
-     */
-    css: [],
-    /*
-     ** Plugins to load before mounting the App
-     ** https://nuxtjs.org/guide/plugins
-     */
-    plugins: [],
-    /*
-     ** Auto import components
-     ** See https://nuxtjs.org/api/configuration-components
-     */
-    components: true,
-    /*
-     ** Nuxt.js dev-modules
-     */
-    buildModules: [
-        // Doc: https://github.com/nuxt-community/eslint-module
-        '@nuxtjs/eslint-module',
-    ],
-    /*
-     ** Nuxt.js modules
-     */
-    modules: [
-        // Doc: https://github.com/nuxt-community/modules/tree/master/packages/bulma
-        '@nuxtjs/bulma',
-        // Doc: https://axios.nuxtjs.org/usage
-        '@nuxtjs/axios',
-    ],
-    /*
-     ** Axios module configuration
-     ** See https://axios.nuxtjs.org/options
-     */
-    axios: {},
-    /*
-     ** Build configuration
-     ** See https://nuxtjs.org/api/configuration-build/
-     */
-    build: {
-        postcss: {
-            preset: {
-                features: {
-                    customProperties: false,
-                },
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            {
+                rel: 'stylesheet',
+                href:
+                    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
             },
-        },
+            {
+                rel: 'stylesheet',
+                href:
+                    'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
+            },
+        ],
+    },
+    css: [{ src: 'bulma/bulma.sass', lang: 'sass' }],
+    plugins: [],
+    components: true,
+    buildModules: ['@nuxtjs/style-resources', '@nuxtjs/eslint-module'],
+    styleResources: {
+        scss: ['~assets/scss/main.scss'],
+    },
+    modules: ['@nuxtjs/axios'],
+    axios: {},
+    build: {
+        // postcss: {
+        //     preset: {
+        //         features: {
+        //             customProperties: false,
+        //         },
+        //     },
+        // },
     },
 }
